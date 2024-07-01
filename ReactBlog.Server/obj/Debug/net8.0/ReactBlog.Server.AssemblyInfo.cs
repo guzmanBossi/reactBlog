83272f9e1417aa53780f6954b8933e3150229e2d
guzman.bossi <guzman.bossi@gmail.com>
2024-07-01 17:04:54 +0200

    wip

--- ReactBlog.Server/obj/Debug/net8.0/ReactBlog.Server.AssemblyInfo.cs
+++ ReactBlog.Server/obj/Debug/net8.0/ReactBlog.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactBlog.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3638b68251398d941d9c0120a00351e8fa419f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactBlog.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactBlog.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
